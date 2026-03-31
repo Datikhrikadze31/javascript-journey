4c00af941a7baef67563795ca83e435928a1aa64
Dati Khrikadze <d.khrikadze@myauto.ge>
2026-04-01 01:26:42 +0400

    Day 4 functions, fucntions inside fucntions if else logics

--- script.js
+++ script.js
@@ -1,34 +1,80 @@
-// DAY 3  Objects
+// // day 4
 
-// let car = {
-//   brand: "MERCEDES",
-//   price: 250000,
-//   year: 2021,
-// };
+// function showMessage() {
+//   console.log("Learning javascipt with my friend gpt");
+// }
 
-// console.log(car.brand);
-// console.log(car.year);
+// // showMessage();
+// // showMessage();
+// // showMessage();
 
-// let user = {
-//   name: "Dati",
-//   age: 24,
-// };
+// // // function showPrice(price) {
+// // //   console.log("Car price is " + price);
+// // // }
 
-// user.age = 25;
-// user.job = "Frontend Developer";
+// // // showPrice(25000);
+// // // showPrice(30000);
+// // // showPrice(40000);
 
-// console.log(user);
+// // function showCar(brand, price) {
+// //   console.log(brand + " costs " + price);
+// // }
 
-let car = [
-  {
-    brand: "BWM",
-    price: 20000,
-  },
-  {
-    brand: "MERCEDES",
-    price: 30000,
-  },
-];
+// // showCar("BMW", 25000);
+// // showCar("MERCEDES", 30000);
 
-console.log(car[0].brand);
-console.log(car[1].price);
+// // function calculatePrice(price, tax) {
+// //   let total = price + tax;
+// //   return total;
+// // }
+
+// // let outcome = calculatePrice(25000, 15);
+// // console.log(outcome);
+
+// // function calculateDiscount(price, discount) {
+// //   let total = price - discount;
+// //   return total;
+// // }
+// // let finalPrice = calculateDiscount(3000, 500);
+// // console.log(finalPrice);
+
+// // function calculateTotalPrice(price, tax, discount) {
+// //   let discountedPrice = price + tax - discount;
+// //   return discountedPrice;
+// // }
+
+// // let bigSale = calculateTotalPrice(20000, 1000, 500);
+// // console.log(bigSale);
+
+// function add(a, b) {
+//   let number = a + b;
+//   return number;
+// }
+
+// function multiply(number) {
+//   let result = number * 3;
+//   return result;
+// }
+// let finalNumber = multiply(add(5, 5));
+
+// console.log(finalNumber);
+
+function checkCarPrice(price) {
+  if (price > 2000) {
+    console.log("Exspenisve car");
+  } else {
+    console.log("Cheap car");
+  }
+}
+checkCarPrice(1500);
+checkCarPrice(3000);
+
+function checkAge(age) {
+  if (age >= 18) {
+    console.log("You are allowed to drive");
+  } else {
+    console.log("Too young");
+  }
+}
+checkAge(16);
+checkAge(24);
